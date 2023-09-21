36e58dd92548e67f28fe2e4e44dba74271a8a930
Jason Raede <jason.raede@gmail.com>
2023-09-21 10:17:52 -0400

    Set position to FULL for SST

--- test_whg.py
+++ test_whg.py
@@ -1,5 +1,5 @@
 from src.whitehat import create_account, Registration
 
-reg = Registration.find_by_id('64f0ef06366f5e8bd04bce75')
+reg = Registration.find_by_id('64ed06315db7536320ecac12')
 
 create_account(reg)
